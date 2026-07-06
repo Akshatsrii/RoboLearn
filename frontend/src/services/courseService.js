4b000d6e91d6b8b69a39cde1e3a21983be48c437
Akshat Srivastava <chikusrivastava535@gmail.com>
2026-07-06 12:01:20 +0530

    Integrate course management with backend APIs

--- frontend/src/services/courseService.js
+++ frontend/src/services/courseService.js
@@ -3,3 +3,4 @@ import axios from "axios";
 const API_BASE = import.meta.env.VITE_API_URL || "/api";
 
 export const getCourses = (params = {}) => axios.get(`${API_BASE}/courses`, { params });
+export const proposeCourse = (data) => axios.post(`${API_BASE}/courses/propose`, data);
